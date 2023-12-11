2d1955a312ce12aae7f516f37ebe25ed5efebafc
pabcrudel <pabcrudel@gmail.com>
2023-12-11 13:34:28 +0100

    change: Game board is now a matrix

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,62 @@
 import { useState } from "react"
+import { cloneDeep } from "lodash"
 
 /** Object containing 2 players and its icon
  * It's name is in Upper Case because it's value never changes
 */
 const TURN = {x: 'x', o: 'o'}
 
+const INITIAL_BOARD = [
+  Array(3).fill(null),
+  Array(3).fill(null),
+  Array(3).fill(null)
+]
+
+const FIRST_TURN_OWNER = TURN.x
+
 function App() {
-  // Midudev called this variables "states".
-  /** An array of objects that will store game status
-   * - At the beginning, is an array on `null`
-   * - At the end could contain:
-   *  [
-   *    o, x, null,
-   *    o, x, null,
-   *    null, x, null
-   *  ]
+  /** Midudev called this variables "states".
    * - [<variable name>, <fn to change it>]
    * - useState() <= Allows to change the variable and sets a default value
   */
-  const [board, setBoard] = useState(Array(9).fill(null))
+  /** An array of objects that will store game status
+   * - At the beginning, is a 3x3 matrix filled with `null`:
+   *  [
+   *    [null, null, null],
+   *    [null, null, null],
+   *    [null, null, null]
+   *  ]
+   * - At the end could contain:
+   *  [
+   *    [x, null, null],
+   *    [null, x, null],
+   *    [o, o, x]
+   *  ]
+  */
+  const [board, setBoard] = useState(INITIAL_BOARD)
 
-  const [turnOwner, setTurnOwner] = useState(TURN.x)
+  const [turnOwner, setTurnOwner] = useState(FIRST_TURN_OWNER)
 
   const [isFinish, setIsFinish] = useState(false)
 
   const [winner, setWinner] = useState(null)
 
-  function updateBoard(position) {
+  function updateBoard(row, column) {
     // Can't override positions that were selected before
-    if (board[position]) return
+    if (board[row][column]) return
 
-    /** It's a bad idea to mutate directly the state:
-     * board[0] = 'x'
-     * instead of that, a better approach is to create a new value and use the
-     * mutation function (2nd item on the array)
-     * To do so, as the board contains primitives, it can be cloned using spread
-     * operator
+    /** It's a bad idea to mutate directly the state. It's better to create a
+     * new value and use the mutation function (2nd item on the array)
      */
-    const newBoard = [...board]
+
+    /** Using `cloneDeep` from `Lodash` to copy recursively */
+    const newBoard = cloneDeep(board)
 
     // Set the turn owner icon on this position and update the board
-    newBoard[position] = turnOwner
+    newBoard[row][column] = turnOwner
     setBoard(newBoard)
 
+    // Checking the current status of the game
     if (checkWinner(newBoard)) {
       setIsFinish(true)
       setWinner(turnOwner)
@@ -56,44 +70,9 @@ function App() {
     setTurnOwner(turnOwner === TURN.x ? TURN.o : TURN.x)
   }
 
-  function checkWinner(board) {
-    // Check rows
-    for (let i = 0; i <= 9; i += 3) {
-      if (check(i, i + 3, 1, board)) return true
-    }
-
-    // Check columns
-    for (let i = 0; i <= 9; i++) {
-      if (check(i, i + 7, 3, board)) return true
-    }
-    
-    // First diagonal
-    if (check(0, 9, 4, board)) return true
-
-    // Second diagonal
-    if (check(2, 7, 2, board)) return true
-
-    return false
-
-    function check(init, fin, update, board) {
-      let lastPiece = board[init];
-
-      if (!lastPiece) return false
-
-      for (let i = init + update; i < fin; i += update) {
-        if (lastPiece !== board[i]) return false
-      }
-      return true
-    }
-  }
-
-  function checkTie(board) {
-    return board.every(position => position !== null)
-  }
-
   function reset() {
-    setBoard(Array(9).fill(null))
-    setTurnOwner(TURN.x)
+    setBoard(INITIAL_BOARD)
+    setTurnOwner(FIRST_TURN_OWNER)
     setIsFinish(false)
     setWinner(null)
   }
@@ -113,15 +92,21 @@ function App() {
         </h2>
         <div className="board">
           {
-            board.map((cellContent, i) =>
-              <button
-                key={i}
-                className='cell'
-                onClick={() => updateBoard(i)}
-                disabled={isFinish}
-              >
-                {cellContent}
-              </button>
+            board.map((row, i) =>
+              <div className="row" key={i}>
+                {
+                  row.map((cellContent, j) =>
+                    <button
+                      key={i + j}
+                      className='cell'
+                      onClick={() => updateBoard(i, j)}
+                      disabled={isFinish}
+                    >
+                      {cellContent}
+                    </button>
+                  )
+                }
+              </div>
             )
           }
         </div>
@@ -129,6 +114,62 @@ function App() {
       </main>
     </>
   )
+}
+
+function checkWinner(board) {
+  const firstValue = board[0][0]
+  if (
+    firstValue && (
+      // First row
+      (firstValue === board[0][1] && firstValue === board[0][2]) ||
+      // First diagonal
+      (firstValue === board[1][1] && firstValue === board[2][2]) ||
+      // First column
+      (firstValue === board[1][0] && firstValue === board[2][0])
+    )
+  ) return true
+  
+  const firstRowSecondCol = board[0][1]
+  if (firstRowSecondCol && (
+      // Second column
+      firstRowSecondCol === board[1][1] && firstRowSecondCol === board[2][1]
+    )
+  ) return true
+
+  const firstRowLastCol = board[0][2]
+  if (
+    firstRowLastCol && (
+      // Last column
+      (firstRowLastCol === board[1][2] && firstRowLastCol === board[2][2]) ||
+      // Last diagonal
+      (firstRowLastCol === board[1][1] && firstRowLastCol === board[2][0])
+    )
+  ) return true
+
+  const secondRowFirstCol = board[1][0]
+  if (secondRowFirstCol && (
+      // Second row
+      (secondRowFirstCol === board[1][1] && secondRowFirstCol === board[1][2])
+    )
+  ) return true
+
+  const lastRowFirstCol = board[2][0]
+  if (lastRowFirstCol && (
+      // Last row
+      (lastRowFirstCol === board[2][1] && lastRowFirstCol === board[2][2])
+    )
+  ) return true
+
+  return false
+}
+
+function checkTie(board) {
+  for (const row of board) {
+    for (const value of row) {
+      if (!value) return false
+    }
+  }
+  return true
 }
 
 export default App
