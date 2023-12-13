e9988350639a645a6784baaf0be9411f105d07d3
pabcrudel <pabcrudel@gmail.com>
2023-12-13 20:18:42 +0100

    change: Use the new import mode

--- test/app.test.jsx
+++ test/app.test.jsx
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
 import { describe, it } from "vitest";
-import { App } from "../src/App"
+import App from "../src/App"
 
 describe('App', () => {
   it('should render', () => {
