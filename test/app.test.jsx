727c729eed705e6ce4526810282883a2b0fc4dff
pabcrudel <pabcrudel@gmail.com>
2023-12-13 21:22:44 +0100

    change: Non JS related content moved to the HTML file

--- test/app.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import { describe, it } from "vitest";
-import App from "../src/App"
-
-describe('App', () => {
-  it('should render', () => {
-    render(<App />)
-  })
-})
--- test/footer.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import { describe, it } from "vitest";
-import Footer from "../src/components/Footer";
-
-describe('Footer', () => {
-  it('should render', () => {
-    render(<Footer />)
-  })
-})
