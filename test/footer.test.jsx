19f41e445cf6b560879b7e2d2c8383c060c8196e
pabcrudel <pabcrudel@gmail.com>
2023-12-13 20:58:11 +0100

    fix: Components path

--- test/footer.test.jsx
+++ test/footer.test.jsx
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
 import { describe, it } from "vitest";
-import Footer from "../src/footer";
+import Footer from "../src/components/Footer";
 
 describe('Footer', () => {
   it('should render', () => {
--- test/tic-tac-toe.test.jsx
+++ test/tic-tac-toe.test.jsx
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
 import { describe, it } from "vitest";
-import TicTacToe from "../src/TicTacToe";
+import TicTacToe from "../src/components/TicTacToe";
 
 describe('TicTacToe', () => {
   it('should render', () => {
